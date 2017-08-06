2629bd82560aa888354c77fce4bac1b4ea268f0a
Ashraf Ali <ashrafali0148@gmail.com>
2017-08-06 09:12:49 +0100

    Update tests

--- lib/device_api/sys_log.rb
+++ lib/device_api/sys_log.rb
@@ -2,38 +2,37 @@ require 'syslog'
 
 module DeviceAPI
   class SysLog
-
     attr_accessor :syslog
 
-    def log(priority,message)
-      if @syslog and @syslog.opened?
+    def log(priority, message)
+      if @syslog && @syslog.opened?
         @syslog = Syslog.reopen('device-api-gem', Syslog::LOG_PID, Syslog::LOG_DAEMON)
       else
         @syslog = Syslog.open('device-api-gem', Syslog::LOG_PID, Syslog::LOG_DAEMON)
       end
 
-      @syslog.log(priority,message)
+      @syslog.log(priority, message)
       @syslog.close
     end
 
     def fatal(message)
-      self.log(Syslog::LOG_CRIT,message)
+      log(Syslog::LOG_CRIT, message)
     end
 
     def error(message)
-      self.log(Syslog::LOG_ERR,message)
+      log(Syslog::LOG_ERR, message)
     end
 
     def warn(message)
-      self.log(Syslog::LOG_WARNING,message)
+      log(Syslog::LOG_WARNING, message)
     end
 
     def info(message)
-      self.log(Syslog::LOG_INFO,message)
+      log(Syslog::LOG_INFO, message)
     end
 
     def debug(message)
-      self.log(Syslog::LOG_DEBUG,message)
+      log(Syslog::LOG_DEBUG, message)
     end
   end
 end
--- spec/execution_spec.rb
+++ spec/execution_spec.rb
@@ -5,11 +5,10 @@ require 'device_api/execution'
 include RSpec
 
 ProcessStatusStub = Struct.new(:exitstatus)
-$STATUS_ZERO = ProcessStatusStub.new(0)
+STATUS_ZERO       = ProcessStatusStub.new(0)
 
 describe DeviceAPI::Execution do
   describe '.execute' do
-
     before(:all) do
       @result = DeviceAPI::Execution.execute('echo boo')
     end
@@ -29,26 +28,23 @@ describe DeviceAPI::Execution do
     it 'capture stderr in hash' do
       expect(@result.stderr).to eq('')
     end
-
   end
 
   describe '.execute_with_timeout_and_retry' do
     it 'should raise an exception if the command takes too long to execute' do
-      stub_const('DeviceAPI::Execution::COMMAND_TIMEOUT',1)
-      stub_const('DeviceAPI::Execution::COMMAND_RETRIES',1)
+      stub_const('DeviceAPI::Execution::COMMAND_TIMEOUT', 1)
+      stub_const('DeviceAPI::Execution::COMMAND_RETRIES', 1)
       sleep_time = 5
-      cmd = "sleep #{sleep_time.to_s}"
+      cmd = "sleep #{sleep_time}"
       expect { DeviceAPI::Execution.execute_with_timeout_and_retry(cmd) }.to raise_error(DeviceAPI::CommandTimeoutError)
     end
 
     it 'should execute successfully if command returns before the timeout' do
-      stub_const('DeviceAPI::Execution::COMMAND_TIMEOUT',2)
-      stub_const('DeviceAPI::Execution::COMMAND_RETRIES',5)
+      stub_const('DeviceAPI::Execution::COMMAND_TIMEOUT', 2)
+      stub_const('DeviceAPI::Execution::COMMAND_RETRIES', 5)
       sleep_time = 1
-      cmd = "sleep #{sleep_time.to_s}"
+      cmd = "sleep #{sleep_time}"
       DeviceAPI::Execution.execute_with_timeout_and_retry(cmd)
     end
-
   end
-
 end
